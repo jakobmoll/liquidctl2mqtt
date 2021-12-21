bda325fcd61385d7f6f0ded98c774e50ad42b707
Jakob Moll <jakobmoll@gmail.com>
2021-12-21 20:27:07 +0100

    Improved "error handling" - it not good.

--- index.js
+++ index.js
@@ -3,44 +3,73 @@ let shell = require('shelljs');
 let fs = require('fs');
 let config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
+let debug = false;
+
 function getLiquidCtlOutput() {
   return new Promise((resolve, reject) => {
     let command = config.liquidctlPath + " " + config.liquictlArgs;
-    console.log(command);
+    // console.log(command);
     let child = shell.exec(command, {async: true, silent: true});
 
+    child.stderr.on('data', function (data) {
+      if (debug)
+        console.log('something went wrong');
+      reject(data);
+    })
+
     child.stdout.on('data', function (data) {
+      if (debug)
+        console.log(data);
       //TODO reject on erogenous data from cmd line
       let obj = JSON.parse(data);
+      // we only want the status section
       let stati = obj[0].status;
-      // console.log(stati);
+      if (debug) console.log(stati);
       let newStati = stati.map(item => {
-        // console.log(item);
         return convertStatusEntry(item)
       });
-      console.log(newStati);
-      resolve(newStati);
+      // convert array of objects to object with key/value
+      let haFriendly = Object.assign({}, ...newStati);
+      if (debug)
+        console.log(haFriendly)
+      resolve(haFriendly);
     });
   }
 )
 }
 
+/**
+ * remove the unit, and sanitize keys
+ * @param item
+ * @returns {any}
+ */
 function convertStatusEntry(item) {
+  //remove "+ and replace space with _
   let newKey = item.key.replaceAll(' ', '_').replaceAll('+','');
-  let newString = `{"${newKey}" : "${item.value} ${item.unit}"}`;
+  let newString = `{"${newKey}" : "${item.value}"}`;
   return JSON.parse(newString);
 }
 
 async function requestListener(req, res) {
-  // res.setEncoding('utf8');
-  const output = await getLiquidCtlOutput();
+  let output;
+  try {
+    output = await getLiquidCtlOutput()
+  } catch (e) {
+    // retry
+    if (debug) {
+      console.log(e);
+      console.log('got error - retrying');
+      console.log('output: ' + output);
+    }
+    output = await getLiquidCtlOutput();
+  }
   res.writeHead(200, {"Content-Type": "application/json"})
   res.write(JSON.stringify(output));
   res.end();
 }
 
 const server = http.createServer(requestListener);
-server.listen(8000);
+server.listen(8001);
 
 
 // async function test()
